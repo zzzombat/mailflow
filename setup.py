9f3d1704d39fe02a014880e9062092426b163d92
zzzombat <zzzombat@gmail.com>
2013-08-31 14:59:29 +0400

    Add requirements to setup script

--- setup.py
+++ setup.py
@@ -5,7 +5,17 @@ sys.path.insert(0, 'src')
 import mailflow
 
 requires = [
-    'flask',
+    'flask==0.9',
+    'flask-security',
+    'sqlalchemy==0.7.9',
+    'flask-sqlalchemy==0.16',
+    'sqlalchemy-migrate',
+    'flask-whooshalchemy==0.54a',
+    'flask-wtf',
+    'pytz==2013b',
+    'flask-babel==0.8',
+    'flup',
+    'flask-admin'
 ]
 
 setup(
