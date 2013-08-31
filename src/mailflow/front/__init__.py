b97f9641a306fff1cab7af4c9e344b5ec8281283
zzzombat <zzzombat@gmail.com>
2013-08-31 14:47:53 +0400

    Add base admin view

--- src/mailflow/front/__init__.py
+++ src/mailflow/front/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
+from flask.ext.admin import Admin
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.security import Security, SQLAlchemyUserDatastore, login_required
 
-
 app = Flask(__name__, static_folder='static', static_url_path='/static')
+admin = Admin(app, name='Mailflow')
 app.config.from_object('mailflow.settings')
 db = SQLAlchemy(app)
+
 import views, models
 
 user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
 security = Security(app, user_datastore)
-
 
 def main():
     app.run(debug = True)
