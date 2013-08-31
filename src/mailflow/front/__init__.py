f9b92e22af22c0878107dcb7fce21582e4686731
zzzombat <zzzombat@gmail.com>
2013-08-31 17:02:25 +0400

    Update admin model views

--- src/mailflow/front/__init__.py
+++ src/mailflow/front/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask.ext.admin import Admin
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.security import Security, SQLAlchemyUserDatastore, login_required
+from flask.ext.security import Security, SQLAlchemyUserDatastore
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
-admin = Admin(app, name='Mailflow')
 app.config.from_object('mailflow.settings')
 db = SQLAlchemy(app)
 
-import views, models
+import views, models, admin
 
 user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
 security = Security(app, user_datastore)
