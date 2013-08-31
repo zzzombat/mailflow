14766096820be7367c51fd1229df77ffba57ec3a
Alexander Morozov <numelion@gmail.com>
2013-08-31 14:54:39 +0400

    Added dashboard

--- src/mailflow/front/views.py
+++ src/mailflow/front/views.py
@@ -5,3 +5,7 @@ from mailflow.front import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
