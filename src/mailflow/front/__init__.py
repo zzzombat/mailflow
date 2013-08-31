fb6a575b78e4b5101d925ad67a84d763f1f3a158
Dmitry Makushin <mdimanmail@gmail.com>
2013-08-31 17:00:56 +0400

    Fixed init database error, fixed readme error, added init_db.py

--- src/mailflow/front/__init__.py
+++ src/mailflow/front/__init__.py
@@ -12,15 +12,5 @@ user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
 security = Security(app, user_datastore)
 
 
-@app.before_first_request
-def create_user():
-    db.create_all()
-    user_datastore.create_role(name='admin', description='')
-    user = user_datastore.create_user(email='admin@example.com', password='1234')
-    db.session.commit()
-    admin_role = user_datastore.find_role("admin")
-    user_datastore.add_role_to_user(user, admin_role)
-    db.session.commit()
-
 def main():
     app.run(debug = True)
