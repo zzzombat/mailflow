cd0fec65656caccdc7f03167b741bffaa2d175c7
Matvey Kruglov <kubuzzzz@gmail.com>
2013-08-31 18:42:03 +0400

    Save and parse message in delivery agent.

--- src/mailflow/deliver.py
+++ src/mailflow/deliver.py
@@ -7,23 +7,53 @@ Simple.
 
 import sys
 import logging
+import hashlib
 import argparse
+import pyzmail
+from mailflow.front import models
 
 logger = logging.getLogger(__name__)
 
 def get_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('user')
     parser.add_argument('recipient')
     parser.add_argument('sender')
     return parser.parse_args()
 
+def parse_message(inbox_id, mail_from, rcpt_to, raw_message):
+    parsed_message = pyzmail.PyzMessage.factory(raw_message)
+    message = models.Message()
+    models.db.session.add(message)
+
+    message.inbox_id = inbox_id
+    message.from_addr = mail_from
+    message.to_addr = rcpt_to
+    message.subject = parsed_message.get_subject()
+    for part in parsed_message.mailparts:
+        if not part.is_body:
+            continue
+        if part.type == 'text/plain':
+            message.body_plain = part.get_payload()
+        if part.type == 'text/html':
+            message.body_html = part.get_payload()
+
+    message.source = hashlib.sha1(raw_message).hexdigest()
+    message.get_source_file().write(raw_message)
+    models.db.session.commit()
+    return message
+
 def main():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, filename='/tmp/debug-deliver.log')
     args = get_args()
-    logger.info("message from %s to %s with content: %s", args.sender, args.recipient, sys.stdin.read())
+    
+    inbox = models.Inbox.query.filter(models.Inbox.login==args.user).first()
 
-
-if __name__ == '__main__':
-    main()
-
-
+    logger.info("new message by user '%s' from '%s' to '%s'", args.user, args.sender, args.recipient)
+    try:
+        message = parse_message(inbox.id, args.sender, args.recipient, sys.stdin.read())
+        return 0
+    except Exception, e:
+        logger.exception('error occured during message parsing')
+        print >> sys.stderr, "Internal error"
+        return 1
