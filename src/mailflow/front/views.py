d326abf6e1e503bfffa17697ffb43358a00dda41
Dmitry Ivashenko <dmitrygrand@gmail.com>
2013-09-07 12:37:04 +0400

    MAIL-4 add registration and login

--- src/mailflow/front/views.py
+++ src/mailflow/front/views.py
@@ -1,11 +1,55 @@
-from flask import render_template
-from mailflow.front import app
+from flask import render_template, flash, redirect, session, url_for, request, g
 
-@app.route('/')
-@app.route('/index')
+from mailflow.front import app, db, models, lm
+from forms import LoginForm, RegistrationForm
+
+from flask.ext.login import login_user, logout_user, current_user, login_required
+
+
+@app.route('/test')
+@login_required
+def test():
+    user = g.user
+    return render_template('test.html', user=user)
+
+
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = models.User.query.filter_by(email = form.email.data).first()
+        session['remember_me'] = form.remember_me.data
+        login_user(user, remember = session['remember_me']  )
+        return redirect('/test')
+    return render_template('index.html', form = form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    user = g.user
+    return render_template('dashboard.html', user=user)
+
+
+@app.route('/reg', methods = ['GET', 'POST'])
+def reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():   
+        user = models.User(email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()    
+        return redirect('/test')
+    return render_template('reg.html', form = form)
+
+@lm.user_loader
+def load_user(id):
+    return models.User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
