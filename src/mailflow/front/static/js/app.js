408002a28df5ad6d8ac0bb7dc4fb1df38d5ca8a6
Александр Цветков <sickuenser@gmail.com>
2013-09-07 15:38:35 +0400

    Added message info page

--- src/mailflow/front/static/js/app.js
+++ src/mailflow/front/static/js/app.js
@@ -3,5 +3,6 @@ angular.module('dashboard', ['inboxServices']).
         $routeProvider.
             //when('', {templateUrl: '/static/templates/dashboard_inboxes.html', controller: DashboardInboxesCtrl}).
             when('/:inboxId', {templateUrl: '/static/templates/dashboard_inbox.html', controller: DashboardInboxCtrl}).
+            when('/:inboxId/message/:messageId', {templateUrl: '/static/templates/dashboard_message.html', controller: MessageInfoCtrl}).
             otherwise({redirectTo: ''});
     }]);
