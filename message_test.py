4edc1177034f62649a0538c2ac6b137008a2db9c
Александр Цветков <sickuenser@gmail.com>
2013-09-07 13:08:41 +0400

    Fixed dashboard and message list controllers and added more test data

--- message_test.py
+++
@@ -1,2 +0,0 @@
-from mailflow import messages
-messages.new_email(12345, 1234)
--- src/mailflow/front/static/js/controllers.js
+++ src/mailflow/front/static/js/controllers.js
@@ -6,15 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function DashboardInboxesCtrl($scope, $http) {
-    $scope.textR = 'dsfsdfsdfsdf';
-    $scope.data = 123;
-
-    $http.get('/static/js/inboxesData.json').success(function (data) {
-        $scope.inboxes = data;
-    });
+function DashboardInboxesCtrl($scope, $http, Inboxes) {
+    $scope.inboxes = Inboxes.get();
 }
 
-function DashboardInboxCtrl($scope, $routeParams, Inbox) {
+function DashboardInboxCtrl($scope, $routeParams, Messages, Inbox) {
     $scope.inbox = Inbox.get({inboxId: $routeParams.inboxId});
+    $scope.messages = Messages.get({inboxId: $routeParams.inboxId});
 }
